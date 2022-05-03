c839c46881a321dce3e07cdb99f300783d85c3b7
Kenneth King <kking935@vt.edu>
2022-05-03 19:56:38 -0400

    changed sign_asset_create_txn.ts to use new blockin stuff

--- src/wallet/sign_asset_create_txn.ts
+++ src/wallet/sign_asset_create_txn.ts
@@ -1,55 +1,14 @@
-import { makeAssetCreateTxn, sha256 } from '../../blockin'
-import algosdk, { waitForConfirmation } from "algosdk";
-import { formatJsonRpcRequest } from "@json-rpc-tools/utils";
+import { createAssetTxn, sendTxn, sha256 } from 'blockin'
 import WalletConnect from '@walletconnect/client';
+import { createWCRequest } from './connect';
 
 export const signAssetCreateTxn = async (connector: WalletConnect, assetAuthorization: string) => {
-    //Generate an opt in tx
-    // Sign transaction
-    // txns is an array of algosdk.Transaction like below
-    // i.e txns = [txn, ...someotherTxns], but we've only built one transaction in our case
-    console.log("SHA 256")
-    console.log(connector.accounts[0] + "_SITESECRET_" + assetAuthorization)
-    console.log(await sha256(connector.accounts[0] + "_SITESECRET_" + assetAuthorization))
-    const txns = [await makeAssetCreateTxn(connector.accounts[0], "Blockin", "AUTH", 1, "blockin-demo", await sha256(assetAuthorization))]
-    const txnsToSign = txns.map(txn => {
-        const encodedTxn = Buffer.from(algosdk.encodeUnsignedTransaction(txn)).toString("base64");
-
-        return {
-            txn: encodedTxn,
-            message: 'Description of transaction being signed',
-            // Note: if the transaction does not need to be signed (because it's part of an atomic group
-            // that will be signed by another party), specify an empty singers array like so:
-            // signers: [],
-        };
-    });
-
-    const requestParams = [txnsToSign];
-    const request = formatJsonRpcRequest("algo_signTxn", requestParams);
-    const result: Array<string | null> = await connector.sendCustomRequest(request);
-    const decodedResult = result.map(element => {
-        return element ? new Uint8Array(Buffer.from(element, "base64")) : null;
-    });
-    console.log("Signed TXN")
-    console.log(decodedResult)
-    const algodServer = "https://testnet-algorand.api.purestake.io/ps2";
-
-    const port = "";
-    const token = {
-        "x-api-key": "H4sefDbnoL8GO8ooRkxQM6CePHih5XDQ405mcBKy" // fill in yours
-    };
-
-    if (decodedResult != null) {
-        const stxs: Uint8Array[] = []
-        decodedResult.forEach((elem) => {
-            if (elem != null) {
-                stxs.push(elem)
-            }
-        });
-        const algodClient = new algosdk.Algodv2(token, algodServer, port);
-        const sendTx = await algodClient.sendRawTransaction(stxs).do();
-        await waitForConfirmation(algodClient, sendTx.txId, 1000);
-
-        console.log("Transaction : " + sendTx.txId);
-    }
+    // Create asset, sign, and send to network
+    const unsignedCreateAssetTxn = await createAssetTxn({
+        from: connector.accounts[0],
+        assetMetadata: await sha256(assetAuthorization),
+    })
+    const wcResult = await createWCRequest([unsignedCreateAssetTxn])
+    const signedCreateAssetTxn = await connector.sendCustomRequest(wcResult)
+    const result = await sendTxn(signedCreateAssetTxn, unsignedCreateAssetTxn.txnId) // this could be done without Blockin
 }
