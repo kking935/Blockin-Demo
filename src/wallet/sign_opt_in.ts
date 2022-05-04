64a0ccced02bf85a84f47f21ae2378ebce822f09
Kenneth King <kking935@vt.edu>
2022-05-03 22:38:37 -0400

    replaced old code with new blockin sign opt in code

--- src/wallet/sign_opt_in.ts
+++ src/wallet/sign_opt_in.ts
@@ -3,49 +3,14 @@ import { createAssetOptInTxn, sendTxn } from 'blockin'
 import { createWCRequest } from "../WalletConnect";
 
 export const signOptIn = async (connector: WalletConnect, assetId: string) => {
-    //Generate an opt in tx
-    // Sign transaction
-    // txns is an array of algosdk.Transaction like below
-    // i.e txns = [txn, ...someotherTxns], but we've only built one transaction in our case
-    const txns = [await makeAssetOptInTxn(connector.accounts[0], Number(assetId))]
-    const txnsToSign = txns.map(txn => {
-        const encodedTxn = Buffer.from(algosdk.encodeUnsignedTransaction(txn)).toString("base64");
+    let uTxn = await createAssetOptInTxn({
+        to: connector.accounts[0],
+        assetIndex: Number(assetId)
+    })
 
-        return {
-            txn: encodedTxn,
-            message: 'Description of transaction being signed',
-            // Note: if the transaction does not need to be signed (because it's part of an atomic group
-            // that will be signed by another party), specify an empty singers array like so:
-            // signers: [],
-        };
-    });
+    const wcRequest = await createWCRequest([uTxn])
+    const signedTxn = await connector.sendCustomRequest(wcRequest);
 
-    const requestParams = [txnsToSign];
-    const request = formatJsonRpcRequest("algo_signTxn", requestParams);
-    const result: Array<string | null> = await connector.sendCustomRequest(request);
-    const decodedResult = result.map(element => {
-        return element ? new Uint8Array(Buffer.from(element, "base64")) : null;
-    });
-    console.log("Signed TXN")
-    console.log(decodedResult)
-    const algodServer = "https://testnet-algorand.api.purestake.io/ps2";
-
-    const port = "";
-    const token = {
-        "x-api-key": "H4sefDbnoL8GO8ooRkxQM6CePHih5XDQ405mcBKy" // fill in yours
-    };
-
-    if (decodedResult != null) {
-        const stxs: Uint8Array[] = []
-        decodedResult.forEach((elem) => {
-            if (elem != null) {
-                stxs.push(elem)
-            }
-        });
-        const algodClient = new algosdk.Algodv2(token, algodServer, port);
-        const sendTx = await algodClient.sendRawTransaction(stxs).do();
-        await waitForConfirmation(algodClient, sendTx.txId, 100)
-
-        console.log("Transaction : " + sendTx.txId);
-    }
+    const sentTxn = await sendTxn(signedTxn, uTxn.txnId)
+    console.log("Transaction : " + uTxn.txnId);
 }
