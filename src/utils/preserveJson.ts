4b0e612b49e5362238e02a343c7fedb48d7b5c49
Kenneth King <kking935@vt.edu>
2022-05-05 00:12:25 -0400

    ignore typescript error, for now

--- src/utils/preserveJson.ts
+++ src/utils/preserveJson.ts
@@ -8,6 +8,7 @@ export const parse = (str: string) => {
         try {
             if ("flag" in value && value.flag === FLAG_TYPED_ARRAY) {
                 // if found, we convert it back to a typed array
+                // @ts-ignore
                 return new context[value.constructor](value.data);
             }
         } catch (e) { }
@@ -22,6 +23,7 @@ export const stringify = (obj: any) => {
         if (value && value instanceof Uint8Array) {
             var replacement = {
                 constructor: value.constructor.name,
+                // @ts-ignore
                 data: Array.apply([], value),
                 flag: FLAG_TYPED_ARRAY
             }
