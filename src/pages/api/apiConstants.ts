fdd9c68efa8b332cc21e9156016863bc3d585cb6
trevormil <trevormil@comcast.net>
2022-05-06 16:55:37 -0400

    demoed sign in with blockin button

--- src/pages/api/apiConstants.ts
+++
@@ -1,4 +0,0 @@
-import algosdk from 'algosdk';
-
-export const passphrase = process.env.RESOURCE_PASSPHRASE ? process.env.RESOURCE_PASSPHRASE : '';
-export const myAccount = algosdk.mnemonicToSecretKey(passphrase)
