ace4f1111793191f416cdba4d3b391399cc1c1c1
trevormil <trevormil@comcast.net>
2022-05-07 14:25:58 -0400

    updated for verifyChallenge returning an obj instead of string now

--- src/pages/api/verifyChallenge.ts
+++ src/pages/api/verifyChallenge.ts
@@ -8,7 +8,7 @@ const verifyChallengeRequest = async (req: NextApiRequest, res: NextApiResponse)
     const body = parse(JSON.stringify(req.body)); //little hack to preserve Uint8Arrays
 
     try {
-        const message = await verifyChallenge(
+        const verificationResponse = await verifyChallenge(
             body.txnBytes,
             body.signature,
             {
@@ -16,11 +16,10 @@ const verifyChallengeRequest = async (req: NextApiRequest, res: NextApiResponse)
             }
         );
 
-        return res.status(200).json({ verified: true, message });
+        return res.status(200).json({ verified: true, message: verificationResponse.message });
     } catch (err) {
         return res.status(200).json({ verified: false, message: err });
     }
-
 };
 
 export default verifyChallengeRequest;
